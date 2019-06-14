1e7a22e85e41cb432b6dad1d22b0a80464d43d3a
Aleksei Danilov <danilovdev@gmail.com>
2019-06-14 13:54:17 +0800

    Update Contents.swift

--- HashTable/451_Sort_Characters_By_Frequency/Swift/451_Sort_Characters_By_Frequency.playground/Contents.swift
+++ HashTable/451_Sort_Characters_By_Frequency/Swift/451_Sort_Characters_By_Frequency.playground/Contents.swift
@@ -1,12 +1,16 @@
 
 
 func frequencySort(_ s: String) -> String {
+    // 1. fill frequency table
     var table: [Character: Int] = [:]
     for ch in s {
         table[ch, default: 0] += 1
     }
     
+    // 2. sort frequency table descending and get array of sorted tuple [(key: Character, value: Int)]
     let sortedTuples = table.sorted { $0.1 > $1.1 }
+    
+    // 3. reduce array of tuples into string by converting every item in array into part of result string
     let result = sortedTuples.reduce("") { result, tuple in
         var part = ""
         for _ in 0..<tuple.value {
@@ -18,6 +22,7 @@ func frequencySort(_ s: String) -> String {
     return result
 }
 
+// 4. test
 assert(frequencySort("tree") == "eert", "Frequency sort does not work")
 assert(frequencySort("cccaaa") == "cccaaa", "Frequency sort does not work")
 assert(frequencySort("Aabb") == "bbAa", "Frequency sort does not work")
