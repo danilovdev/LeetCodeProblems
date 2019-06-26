2303e13e9080c796dc7e0f0d77226a18aec51d4a
Alexey Danilov <alexeydanilov@MacBook-Pro-Alexey.local>
2019-06-26 11:54:02 +0800

    100. Same Tree.Swift Solution

---
+++ Tree/100_Same_Tree/100_Same_Tree.playground/Contents.swift
@@ -0,0 +1,19 @@
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+}
+
+// Recursive Solution
+func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+    if p == nil && q == nil { return true }
+    if p == nil || q == nil { return false }
+    return p?.val == q?.val &&
+        isSameTree(p?.left, q?.left) &&
+        isSameTree(p?.right, q?.right)
+}
