b427a9e64f4d1ccea9e63655ecb13260f7f656cd
Aleksei <danilovdev@gmail.com>
2019-08-19 13:28:34 +0800

    404. Sum of Left Leaves.Swift and Java solutions

---
+++ Tree/404_Sum_of_Left_Leaves/Swift/404_Sum_of_Left_Leaves.playground/Contents.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+class Solution {
+    func sumOfLeftLeaves(_ root: TreeNode?) -> Int {
+        guard let root = root else {
+            return 0
+        }
+        if (root.left != nil && root.left?.left == nil && root.left?.right == nil) {
+            return root.left!.val + sumOfLeftLeaves(root.right);
+        } else {
+            return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+        }
+    }
+}
